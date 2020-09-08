2f27fea0e6421ab73a92d22c4bd55fc86b1507e8
yxk1023 <510261743@qq.com>
2020-09-08 17:48:04 +0800

    0908

--- python/2020-09-08.py
+++ python/2020-09-08.py
@@ -5,3 +5,24 @@ def power(x,n=2):
         s=s*x
     return s
 print(power(5))
+def enroll (name ,gender,age=5,city='shanghai'):
+    print('name:{}'.format(name))
+    print('gender:{}'.format(gender))
+    print('age:{}'.format(age))
+    print('city:%s',city)
+enroll('nima','女',city='beijing',age=66)
+
+# def add_end (l=[]):
+#     l.append('end')
+#     return l
+# print(add_end([1,'ss',3]))
+# print(add_end())
+# print(add_end())
+
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+print(add_end())
+print(add_end())

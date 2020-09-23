98ad77acd539bcf64e9225059312cd4bc316ed16
yxk1023 <510261743@qq.com>
2020-09-23 21:25:52 +0800

    2020-09-23

---
+++ python/2020-09-22.py
@@ -0,0 +1,51 @@
+# d = {'apple':1,'s':22,'a':'a'}
+# d['apple']=2
+# print(d['apple'])
+# print(d.get('s123'))
+# d.pop('apple')
+# print(d)
+#
+# s= set([1,2,33,33,22,2,3])
+# print(s)
+# s.add(222)
+# print(s)
+# s.remove(1)
+# print(s)
+#
+# s1 = set([1,2,3])
+# s2 = set([2,3,4])
+# print(s1&s2)
+# print(s1)
+
+
+# a ={ 'apple':1,'banana':2,'melone':3}
+# print(a)
+
+# a = ['1','s','a','b','ab']
+# a.sort()
+# print(a)
+# print(len(a))
+# j = 0
+# for i in range(10):
+#     j= j+1
+#     print(j)
+
+#数组排序
+# a = [15,11,33222222,52,61,43]
+# i = 0
+# j = 1
+# x = 0
+# for i in range(6):
+#     for (j) in range(i,6):
+#         if a[i]>a[j]:
+#             x = a[i]
+#             a[i] = a[j]
+#             a[j] = x
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+
+# a = 'abc'
+# b = a.replace('a','A')
+# print(a)
+# print(b)

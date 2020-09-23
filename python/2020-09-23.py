c75d15f9069cdc704723127d52fccfdb276d29ef
yxk1023 <510261743@qq.com>
2020-09-23 22:28:08 +0800

    2020-09-23

---
+++ python/2020-09-23.py
@@ -0,0 +1,51 @@
+# a ={ 'apple':1,'banana':2,'melone':3}
+# print(a)
+
+# a = ['1','s','a','b','ab']
+# a.sort()
+# print(a)
+# print(len(a))
+# j = 0
+# for i in range(10):
+#     j= j+1
+#     print(j)
+
+#数组排序
+# a = [15,11,33222222,52,61,43]
+# i = 0
+# j = 1
+# x = 0
+# for i in range(6):
+#     for (j) in range(i,6):
+#         if a[i]>a[j]:
+#             x = a[i]
+#             a[i] = a[j]
+#             a[j] = x
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+
+# a = 'abc'
+# b = a.replace('a','A')
+# print(a)
+# print(b)
+
+# a = '123'
+# print(int(a))
+# print(float(a))
+#
+# print(bool('1'))
+# print(str(100))
+#
+# a = max
+# print(a(1,2))
+# print(hex(12))
+import math
+from Abs1 import myabs
+
+print(myabs(int('-1')))
+
+a = (10)
+print(a)
+del a
+print(a)

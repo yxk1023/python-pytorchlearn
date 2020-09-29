a614967128f19fb8bf3fe9db5a954e58fee55655
yxk1023 <510261743@qq.com>
2020-09-29 17:11:22 +0800

    2020-09-29

--- python/2020-09-28.py
+++ python/2020-09-28.py
@@ -30,10 +30,10 @@ import torch
 #     return 'done'
 #
 # feibo1(6)
-
-#这里，最难理解的就是generator和函数的执行流程不一样。函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
-# 而变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行
-
+'''
+这里，最难理解的就是generator和函数的执行流程不一样。函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
+ 而变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行
+'''
 # def odd():
 #     print('第一步')
 #     yield 1
---
+++ python/2020-09-29.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas
+a = 'a'
+print('变量名：'+a)
+print('变量名：',a)
